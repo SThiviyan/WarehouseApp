6d0581f79ae6ea8fb9a5094270587a2a4f6581e0
Thiviyan <thivi30082006@outlook.com>
2025-04-03 23:40:24 +0100

    first fetching method added, connecting to server works for simple get requests

--- WarehouseApp/backend/Database.swift
+++ WarehouseApp/backend/Database.swift
@@ -13,8 +13,17 @@ import Foundation
 //either coredata (not crossplatform) or sqlite or firebase
 
 struct Database{
+
+
+    let authenticationUrl: URL = URL(string: "http://localhost:8080/api/authenticate")!
+    let userInfoUrl: URL = URL(string: "http://localhost:8080/api/userinfo")!
+    let unitsUrl: URL = URL(string: "http://localhost:8080/api/units")!
+    let categoriesUrl: URL = URL(string: "http://localhost:8080/api/categories")!
+    let productsUrl: URL = URL(string: "http://localhost:8080/api/products")!
+
     
-    func authenticate() -> Bool {
+    
+    static func authenticate() -> Bool {
         
         var request = URLRequest(url: URL(string: "http://localhost:8080/api")!)
         request.httpMethod = "GET"
@@ -46,6 +55,44 @@ struct Database{
     static func getProduct(withBarcode barcode: String) -> Product? {
         return nil
     }
+    
+    
+    
+    
 
+    
+    
+    
+    static func fetchCategories() async {
+       
+    }
+    
+    
+   static func fetchUserInfo() async {
+        
+   }
+    
+   static func fetchUnits() async -> [unit]{
+        
+        if let url = URL(string: "http://localhost:8080/api/units") {
+            do{
+                let(data, _) = try await URLSession.shared.data(from: url)
+                
+                let decoder = JSONDecoder()
+                let units = try decoder.decode([unit].self, from: data)
+                
+                print("Units data: \(units)")
+                
+                return units
+            }
+            
+            catch
+            {
+                print("GET UNITS ERROR", error)
+            }
+        }
+       
+       return []
+    }
     
 }
