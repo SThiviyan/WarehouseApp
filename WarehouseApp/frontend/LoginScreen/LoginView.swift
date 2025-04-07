82708a253e0b9ba3670348613ba72738550ddd3d
Thiviyan <thivi30082006@outlook.com>
2025-04-07 11:36:10 +0100

    Intial startup view created

--- WarehouseApp/frontend/LoginScreen/LoginView.swift
+++ WarehouseApp/frontend/LoginScreen/LoginView.swift
@@ -2,42 +2,14 @@
 //  LoginView.swift
 //  WarehouseApp
 //
-//  Created by Thiviyan Saravanamuthu on 06.04.25.
+//  Created by Thiviyan Saravanamuthu on 07.04.25.
 //
 
 import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        NavigationStack{
-            VStack{
-                
-                HStack{
-                    Image(systemName: "")
-                    Text("Your Products in one convenient spot.")
-                }
-                HStack
-                {
-                    Image(systemName: "")
-                    Text("")
-                }
-                HStack
-                {
-                    Image(systemName: "")
-                    Text("")
-                }
-                
-                Button("continue", action: {
-                    
-                })
-                .frame(width: 300, height: 60)
-                .background(Color.blue)
-                .clipShape(Capsule())
-                .buttonStyle(.borderedProminent)
-                .bold()
-            }
-            .navigationTitle("Welcome to Warehouse")
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
---
+++ WarehouseApp/frontend/LoginScreen/StartupView.swift
@@ -0,0 +1,103 @@
+//
+//  LoginView.swift
+//  WarehouseApp
+//
+//  Created by Thiviyan Saravanamuthu on 06.04.25.
+//
+
+import SwiftUI
+
+struct StartupView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                
+                Spacer()
+                HStack{
+                
+                    Text("Willkommen bei Warehouse")
+                        .padding()
+                        .font(.largeTitle)
+                    Spacer()
+                }
+                .padding()
+                .padding(.bottom, -60)
+                                
+                HStack(alignment: .center){
+                    
+                    Spacer()
+                    
+                    VStack{
+                        
+                        Spacer()
+                        
+                        Image(systemName: "door.garage.open")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.blue)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "vial.viewfinder")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.blue)
+                            
+                        
+                        Spacer()
+                        
+                        Image(systemName: "sparkle.magnifyingglass")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.blue)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                
+                    VStack(alignment: .leading){
+                        
+                        Spacer()
+                        
+                        Text("Alle deine Produkte, gespeichert an einem Ort")
+                            .bold()
+                        
+                        Spacer()
+                        
+                        Text("Scannen, notwendige Informationen eintragen und fertig!")
+                            .bold()
+                        
+                        Spacer()
+                        
+                        Text("Scannen, nachschlagen, und mehr...")
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    .padding()
+
+                    
+                    
+                }
+               // .frame(height: 550)
+                
+                
+
+                
+                Button("Einloggen", action: {
+                    
+                })
+                .frame(width: 300, height: 60)
+                .background(Color.blue)
+                .clipShape(Capsule())
+                .buttonStyle(.borderedProminent)
+                .bold()
+            }
+        
+        }
+    }
+}
+
+#Preview {
+    StartupView()
+}
