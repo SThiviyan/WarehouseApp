80f1ffd2464f7244f8467dcc1ca1c0d2ec1e664b
Thiviyan <thivi30082006@outlook.com>
2025-04-07 20:35:42 +0100

    Register page UI created

--- WarehouseApp/frontend/LoginScreen/Register.swift
+++ WarehouseApp/frontend/LoginScreen/Register.swift
@@ -6,11 +6,123 @@
 //
 
 import SwiftUI
+import GoogleSignInSwift
 
 struct Register: View {
+    @State var emailField: String = ""
+    @State var passwordField: String = ""
+    
+    @Environment(\.colorScheme) var colorScheme
+
+    
+    
+    @State var authfailed: Bool = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            Text("Registrierung")
+                .font(.title)
+                .padding()
+            
+            Group{
+                TextField("Email", text: $emailField)
+                    .textFieldStyle(.automatic)
+                    .padding()
+                SecureField("Passwort", text: $passwordField)
+                    .textFieldStyle(.automatic)
+                    .padding()
+            }
+            .background(Color.gray.opacity(0.1))
+            .clipShape(.buttonBorder)
+            .padding(.leading)
+            .padding(.trailing)
+         
+            
+            Text("Email bereits registriert")
+                .font(.subheadline)
+                .bold()
+                .foregroundStyle(.red)
+                .opacity(authfailed ? 1 : 0)
+            
+                
+            
+            Button(action: {
+                
+                let success = handleSignUp(email: emailField, password: passwordField)
+                
+                if(!success)
+                {
+                    print("Email bereits registriert")
+                    authfailed = true
+                }
+                else
+                {
+                    let defaults = UserDefaults.standard
+                    defaults.set(true, forKey: "LoggedIn")
+                    defaults.set(false, forKey: "FirstLaunch")
+                    
+                    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first {
+                        
+                        let tabbar = getTabbar()
+                        
+                        window.rootViewController = tabbar
+                        window.makeKeyAndVisible()
+                        
+                        UIView.transition(with: window, duration: 0.4, options: [.transitionCrossDissolve], animations: {}, completion: {_ in print("Main App activated")})
+                    }
+                }
+               
+            }, label:{
+                Text("Registrieren")
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .buttonStyle(.borderless)
+                    .tint(Color.white)
+                    .clipShape(.buttonBorder)
+                    .padding()
+            })
+            .padding()
+        
+            
+            Text("oder:")
+                .font(.title3)
+            
+            
+            if(self.colorScheme == .light)
+            {
+                SignInWithAppleView()
+                    .signInWithAppleButtonStyle(.black)
+            }
+            else
+            {
+                SignInWithAppleView()
+                    .signInWithAppleButtonStyle(.white)
+            }
+            
+            GoogleSignInButton(action: {
+                handleGSignIn()
+            })
+            .frame(width: 300, height: 50)
+            .buttonStyle(.plain)
+            
+            Spacer()
+            
+           
+        }
+       
     }
+        
+}
+
+
+
+func handleSignUp(email: String, password: String) -> Bool
+{
+    return true
 }
 
 #Preview {
