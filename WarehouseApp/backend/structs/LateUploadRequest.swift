821f4483cfab3f1f23610f5b6f23008493f03e86
Thiviyan <thivi30082006@outlook.com>
2025-05-24 21:45:10 +0200

    Late upload ability implementation started. LateUploadRequests collect all actions that need to be done on the server

---
+++ WarehouseApp/backend/structs/LateUploadRequest.swift
@@ -0,0 +1,28 @@
+//
+//  LateUploadRequest.swift
+//  WarehouseApp
+//
+//  Created by Thiviyan Saravanamuthu on 24.05.25.
+//
+
+
+import Foundation
+
+enum uploadtype{
+    case POST
+    case DELETE
+}
+
+
+struct LateUploadRequest
+{
+    let type: uploadtype
+    let object: Any
+    let timeStamp: Date
+    
+    init(type: uploadtype, object: Any, timeStamp: Date) {
+        self.type = type
+        self.object = object
+        self.timeStamp = timeStamp
+    }
+}
