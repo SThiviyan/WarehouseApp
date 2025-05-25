8d7cdf32d2d62467cba668c5aee66f6fbeb7a11f
Thiviyan <thivi30082006@outlook.com>
2025-05-25 21:32:57 +0200

    change to AppData struct, lateupload added

--- WarehouseApp/backend/structs/AppData.swift
+++ WarehouseApp/backend/structs/AppData.swift
@@ -14,14 +14,16 @@ struct AppData
     var units: [Unit]
     var UserData: User?
     var currencies: [Currency]
+    var lateRequests: [LateUploadRequest]
     
     
-    init(categories: [Category], products: [Product], units: [Unit], UserData: User, currencies: [Currency]) {
+    init(categories: [Category], products: [Product], units: [Unit], UserData: User, currencies: [Currency], LateUploadRequest: [LateUploadRequest]) {
         self.categories = categories
         self.products = products
         self.units = units
         self.UserData = UserData
         self.currencies = currencies
+        self.lateRequests = LateUploadRequest
     }
     
     init(){
@@ -30,5 +32,6 @@ struct AppData
         units = []
         currencies = []
         UserData = User()
+        lateRequests = []
     }
 }
